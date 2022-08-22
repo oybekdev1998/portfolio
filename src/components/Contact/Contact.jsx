7dc00b5a7077531f9aa4458278ec955cb58a7fac
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-08-22 20:21:17 +0500

    styled contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout';
+import { LocalPhone } from '@mui/icons-material';
+import MailIcon from '@mui/icons-material/Mail';
+import styles from './Contact.module.css'
 
 const Contact = () => {
   return (
-    <div>
+    <div className={styles.contact}>
       <MainLayout />
+      <div className={styles.card}>
+        <ul className={styles.items}>
+          <li className={styles.item}><LocalPhone/> <a href='tel:998993884635'>+998993884635</a></li>
+          <li className={styles.item}><MailIcon/> <a href='mailto:oybekjon.shamuratov@mail.ru'>oybekjon.shamuratov@mail.ru</a></li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
